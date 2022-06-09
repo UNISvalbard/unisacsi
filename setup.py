e2001032c73b99fc80ab8ff3c2681fcd3b025c52
lfrankunis <75121379+lfrankunis@users.noreply.github.com>
2022-06-09 19:00:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ['matplotlib==3.5', 'glob2', 'jupyter', 'copy', 'seabird','numpy','scipy','pandas==1.2', 'geopandas', 'shapely', 'netCDF4','cartopy','gsw','cmocean','requests','adjusttext', 'spyder', 'xarray', 'rioxarray', 'dask', 'utm']
+requirements = ['matplotlib==3.5', 'glob2', 'jupyter', 'seabird','numpy','scipy','pandas==1.2', 'geopandas', 'shapely', 'netCDF4','cartopy','gsw','cmocean','requests','adjusttext', 'spyder', 'xarray', 'rioxarray', 'dask', 'utm']
 
 setup(
   name = 'unisacsi',        
